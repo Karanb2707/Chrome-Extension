2def8ca04ad4a0b5f8ec40ea838d86c1279733b6
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-09-25 23:40:54 +0530

    Changes in linkdin ids

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 document.getElementById('scrapeButton').addEventListener('click', function() {
     const linkedinUrls = [
         'https://www.linkedin.com/in/shilpa-tiwari-79b82b212/',
+        'https://www.linkedin.com/in/proton9/',
         'https://www.linkedin.com/in/prernachavan232/',
-        'https://www.linkedin.com/in/proton9/'
     ];
 
     // Send the list of URLs to the background script
